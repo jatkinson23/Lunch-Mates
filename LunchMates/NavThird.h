0e0b050819d52e3b0df757ea5866a4634f29db00
John Atkinson <YumYumNinja@John-Atkinsons-Mac-Pro.local>
2012-10-21 19:08:00 -0400

    first commit

---
+++ LunchMates/NavThird.h
@@ -0,0 +1,13 @@
+//
+//  NavThird.h
+//  LunchMates
+//
+//  Created by John Atkinson on 9/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavThird : UIViewController
+
+@end
